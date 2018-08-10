4e899628a57faa9afedcb357a191fa03983b9a17
jundycosmod <jundycosmod@gmail.com>
2018-08-10 12:48:09 +0800

    Refs #1 Added roles table migration
    Signed-off-by: jundycosmod <jundycosmod@gmail.com>

---
+++ migrations/m180810_043940_create_roles_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `roles`.
+ */
+class m180810_043940_create_roles_table extends Migration {
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp() {
+        $this->createTable('roles', [
+            'id' => $this->primaryKey(),
+            'role_description' => $this->string(150)->notNull(),
+            'created_at' => $this->timestamp() . ' DEFAULT CURRENT_TIMESTAMP ',
+            'updated_at' => $this->timestamp() . ' DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown() {
+        $this->dropTable('roles');
+    }
+}
