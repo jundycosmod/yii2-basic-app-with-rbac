59cd59da78c429f3b14dd41d1544154e55a4d9a5
jundycosmod <jundycosmod@gmail.com>
2018-08-17 16:03:46 +0800

    Changed landing page message.
    Signed-off-by: jundycosmod <jundycosmod@gmail.com>

--- views/site/index.php
+++ views/site/index.php
@@ -9,7 +9,7 @@ $this->title = Yii::$app->name;
     <div class="row tile_count">
         <div class="jumbotron">
             <h1 class="display-4">Hello, world!</h1>
-            <p class="lead">This project gives developers a skeleton app for a yii2 simple app with user authentication and basic rbac.</p>
+            <p class="lead">This project gives developers a skeleton app for a yii2 basic app with user authentication and basic rbac.</p>
             <hr class="my-4">
             <p>This was created out of frustration to setup RBAC everytime I am creating new Yii2 application.</p>
             <a class="btn btn-primary btn-lg" href="http://jundy.co" role="button">Learn more</a>
